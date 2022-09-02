e3113128dda7232b5552d2f64160075c66e0baab
Paul Makles <paulmakles@gmail.com>
2022-09-02 08:51:52 +0100

    chore: update source code URL

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="flex flex-col w-full items-center text-sm">
             <span>Made by <a href="https://insrt.uk" target="_blank" className="underline">insert#0751</a></span>
-            <span><a href="https://gitlab.insrt.uk/insert/kcl-timetable" target="_blank" className="underline">Source code</a></span>
+            <span><a href="https://github.com/KCLOSS/kcl-timetable" target="_blank" className="underline">Source code</a></span>
         </div>
     )
 }
